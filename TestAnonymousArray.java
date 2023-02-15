993140afaf1f8272aaec3c479522ee2aa24dae57
satyamgkg <97548391+satyamgkg@users.noreply.github.com>
2023-02-15 19:15:02 +0530

    Add files via upload

---
+++ TestAnonymousArray.java
@@ -0,0 +1,15 @@
+//Java Array
+  
+public class TestAnonymousArray
+{  
+static void printArray(int arr[])
+{  
+for(int i=0;i<arr.length;i++)  
+System.out.println(arr[i]);  
+}  
+  
+public static void main(String args[])
+{  
+printArray(new int[]{10,22,44,66});
+}
+}  
